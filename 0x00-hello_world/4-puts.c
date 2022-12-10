b9afda082cabf1a8403141ec337272e4b2d08c3c
Khalid Olabisi <khalidolabisi@gmail.com>
2022-12-10 01:57:21 -0800

    correcting errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ * main - entry point
+ *
+ * Return: 0 after printing the function
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
