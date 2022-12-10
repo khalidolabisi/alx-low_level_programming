9e1cd4bdf33d5b9766827da8a0ed6f09ae6702ad
Khalid Olabisi <khalidolabisi@gmail.com>
2022-12-10 02:12:26 -0800

    correcting errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,16 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korper, 2015-10-19",
- * followed by a new line, to standard error.
- * Return: Always 1.
+ * main -print quote
+ *
+ * Description: prints Dors' quote
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora Korper,2015-10-19\n",
-			59);
+	write(2, "and that piece of art is useful\" -DoraKorpar,2015-10-19\n", 59);
 	return (1);
 }
