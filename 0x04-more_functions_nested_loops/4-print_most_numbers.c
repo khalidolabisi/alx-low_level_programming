8ce5920275b56108bed195dd9315951ed6e6d27f
Khalid Olabisi <khalidolabisi@gmail.com>
2022-12-15 20:09:17 -0800

    some additional tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,6 @@
 void print_most_numbers(void)
 {
 	int i;
-
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
@@ -20,5 +19,4 @@ void print_most_numbers(void)
 		}
 	}
 	_putchar('\n');
-
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,7 +16,6 @@ void print_line(int n)
 	} else
 	{
 		int i;
-
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
