342de57defae5ec73f11953a296b708840c0653c
Khalid Olabisi <khalidolabisi@gmail.com>
2022-12-19 07:36:45 -0800

    first advanced task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, l, f, d;
+
+	i = 0;
+	d = 0;
+	n - 0;
+	l = 0;
+	f = 0;
+	d = 0;
+
+	while (s[l] != '\0')
+		l++;
+	while (i < l && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			d = s[i] - '0';
+			if (d % 2)
+				k = -k;
+			n = n * 10 + k;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
