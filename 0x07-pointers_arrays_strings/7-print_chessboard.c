7172ac9c773102256c973fddc84614c9ac6115bf
Khalid Olabisi <khalidolabisi@gmail.com>
2023-01-02 13:31:22 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: array containing board elements
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int h, l;
+
+	for (h = 0; a[h][7]; h++)
+	{
+		for (l + 0; l < 8; l++)
+			_putchar(a[h][l]);
+		_putchar('\n');
+	}
+}
